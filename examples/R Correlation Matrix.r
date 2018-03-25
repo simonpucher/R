c43ad042e83cb9a8bfcc8ac7809a9a2a680fe43d
Simon Pucher <simonpucher@users.noreply.github.com>
2018-03-25 12:27:04 +0200

    correlation matrix workflow

---
+++ examples/R Correlation Matrix.r
@@ -0,0 +1,61 @@
+# Print correlation matrix in R
+
+#1. install and load
+# install.packages('quantmod')
+# install.packages("corrplot")
+library('quantmod')
+library(corrplot)
+
+#2. params
+# Pay attention currencys and stock do not start at the same day
+TIMEFRAME = '2018-01-05'
+WATCHLIST = c('FB', 'AMZN', 'NFLX', 'GOOG', 'GE', 'XOM')
+getSymbols(WATCHLIST, from=TIMEFRAME)
+
+#3. get some data (you may get a warning here about the hyperparameter auto.assign)
+# amzn = getSymbols('AMZN', src='google', from='2016-06-15', auto.assign=FALSE)
+# AMZN = getSymbols('AMZN', from=TIMEFRAME, auto.assign=FALSE)
+
+#4. quick exploration of quantmod charting capabilities
+# chartSeries(AMZN, TA = c(addSMA(50),addVo(),addBBands()), subset='2016-07::2018-03')
+
+#5. get comparison data for FB, GOOG, NFLX
+# FB = getSymbols('FB', from=TIMEFRAME, auto.assign=FALSE)
+# GOOG = getSymbols('GOOG', from=TIMEFRAME, auto.assign=FALSE)
+# NFLX = getSymbols('NFLX', from=TIMEFRAME, auto.assign=FALSE)
+# GE = getSymbols('GE', from=TIMEFRAME, auto.assign=FALSE)
+# XOM = getSymbols('XOM', from=TIMEFRAME, auto.assign=FALSE)
+## USD_EUR = getSymbols('USD/EUR', from=TIMEFRAME, auto.assign=FALSE, src="oanda")
+
+#6. take a quick look at what the data looks like
+# head(NFLX)
+
+#7. merge data we want into new object, cast as data.frame
+# stocks = as.data.frame(merge(FB$FB.Close, AMZN$AMZN.Close,NFLX$NFLX.Close, GOOG$GOOG.Close, GE$GE.Close, XOM$XOM.Close, USD_EUR$USD.EUR))
+stocks = as.data.frame(merge(FB$FB.Close, AMZN$AMZN.Close,NFLX$NFLX.Close, GOOG$GOOG.Close, GE$GE.Close, XOM$XOM.Close))
+
+#8. see what we have
+# head(stocks)
+
+#9. change column names
+# names(stocks) = c('FB', 'AMZN', 'NFLX', 'GOOG', 'GE', 'XOM', 'USDEUR')
+# names(stocks) = c('FB', 'AMZN', 'NFLX', 'GOOG', 'GE', 'XOM')
+names(stocks) = WATCHLIST;
+
+#10. store daily returns in new dataframe
+# quantmod also has a function periodReturn(stock, period='daily, monthly, etc.') and allReturns()
+stocks_returns = data.frame(diff(as.matrix(log(stocks))))
+
+#11. check transformed data
+# head(stocks_returns)
+
+#12. get correlations
+cor(stocks_returns)
+
+#13. plot scatter matrix, top row second col x = fb, second row first col x = amzn, etc
+plot(stocks_returns)
+
+#14. plot it beauty
+m <- cor(stocks_returns)
+corrplot.mixed(m, order = "hclust", addrect = 2, tl.col = "black", tl.srt = 45)
+#corrplot.mixed(m)
