40773aa5f73c4a77c2f992d7b297abcc2324a79e
Simon Pucher <simonpucher@users.noreply.github.com>
2018-03-26 21:11:11 +0200

    compare many symbols

--- examples/R Correlation Matrix.r
+++ examples/R Correlation Matrix.r
@@ -1,20 +1,21 @@
-# Print correlation matrix in R
+# Print correlation matrix with corrplot in R
 
-#1. install and load
-# install.packages('quantmod')
-# install.packages("corrplot")
+#1. install and load the packages
+if (!require(quantmod)) install.packages('quantmod')
 library('quantmod')
+
+if (!require(corrplot)) install.packages('corrplot')
 library(corrplot)
 
 #2. params
-# Pay attention currencys and stock do not start at the same day
+# Pay attention currencys and stock do not trade always at the same day
 TIMEFRAME = '2018-01-05'
-WATCHLIST = c('FB', 'AMZN', 'NFLX', 'GOOG', 'GE', 'XOM')
-getSymbols(WATCHLIST, from=TIMEFRAME)
+WATCHLIST = c('V','FB', 'AMZN', 'NFLX', 'GOOG', 'GE', 'XOM','CAT','NKE','MMM', 'MCD','HD','DIS')
+ALLSYMBOLS = getSymbols(WATCHLIST, from=TIMEFRAME)
 
 #3. get some data (you may get a warning here about the hyperparameter auto.assign)
 # amzn = getSymbols('AMZN', src='google', from='2016-06-15', auto.assign=FALSE)
-# AMZN = getSymbols('AMZN', from=TIMEFRAME, auto.assign=FALSE)
+# DJI = getSymbols('^DJI', from=TIMEFRAME, auto.assign=FALSE)
 
 #4. quick exploration of quantmod charting capabilities
 # chartSeries(AMZN, TA = c(addSMA(50),addVo(),addBBands()), subset='2016-07::2018-03')
@@ -26,13 +27,15 @@ getSymbols(WATCHLIST, from=TIMEFRAME)
 # GE = getSymbols('GE', from=TIMEFRAME, auto.assign=FALSE)
 # XOM = getSymbols('XOM', from=TIMEFRAME, auto.assign=FALSE)
 ## USD_EUR = getSymbols('USD/EUR', from=TIMEFRAME, auto.assign=FALSE, src="oanda")
+ALLSYMBOLS = getSymbols(WATCHLIST, from=TIMEFRAME)
 
 #6. take a quick look at what the data looks like
 # head(NFLX)
 
 #7. merge data we want into new object, cast as data.frame
 # stocks = as.data.frame(merge(FB$FB.Close, AMZN$AMZN.Close,NFLX$NFLX.Close, GOOG$GOOG.Close, GE$GE.Close, XOM$XOM.Close, USD_EUR$USD.EUR))
-stocks = as.data.frame(merge(FB$FB.Close, AMZN$AMZN.Close,NFLX$NFLX.Close, GOOG$GOOG.Close, GE$GE.Close, XOM$XOM.Close))
+stocks = as.data.frame(merge(V$V.Close , FB$FB.Close, AMZN$AMZN.Close,NFLX$NFLX.Close, GOOG$GOOG.Close, GE$GE.Close, XOM$XOM.Close, CAT$CAT.Close, NKE$NKE.Close, MMM$MMM.Close, MCD$MCD.Close, HD$HD.Close, DIS$DIS.Close))
+
 
 #8. see what we have
 # head(stocks)
